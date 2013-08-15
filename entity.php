eb5ed952ec32e172c868e2212837233f1ada48d3
Tong Yu <yutongoracle@hotmail.com>
2013-08-15 22:03:09 +0800

    search for wanfangdata

---
+++ entity.php
@@ -0,0 +1,31 @@
+<?php
+require_once('appvars.php');
+include_once ("./header.php");
+?>
+<div class="row-fluid marketing">
+    <div class="span6">
+        <div class ="well">
+            <div class="media">
+                <a class="pull-right" href="navigator.php">
+                    <img class="media-object" src="img/resources.jpg" data-src="holder.js/64x64">
+                </a>
+                <div class="media-body"  align ="left">
+                    <h2 class="media-heading">人参</h2>
+                    <p>草药</p>
+                    <p>从知识组织系统、科学数据库、文献库和博客等知识源中搜集中医药知识资源，基于知识图谱实现知识资源的整合。</p>
+                </div>
+            </div>
+            <div  align ="left">
+                <p>Born: July 21, 1977 (age 36), Birdsboro, Pennsylvania, United States</p>
+                <p>Awards: PGA Vanguard Award</p>
+                <p>Spouse: Kathy Clark</p>
+                <p>Spouse: Kathy Clark</p>
+                <p>Parents: Don Hurley</p>
+                <p>Education: Twin Valley High School, Indiana University of Pennsylvania</p>
+            </div>
+        </div>
+    </div>
+</div>     
+<?php
+include_once ("./foot.php");
+?>
--- search.php
+++ search.php
@@ -16,8 +16,8 @@ if (isset($_POST['submit'])) {
             <li><a href="#tab2" data-toggle="tab">成人</a></li>
             <li><a href="#tab3" data-toggle="tab">儿科</a></li>
             <li><a href="#tab4" data-toggle="tab">患者</a></li>
-            <li><a href="#tab4" data-toggle="tab">文献</a></li>
-            <li><a href="#tab5" data-toggle="tab">图表</a></li>
+            <li><a href="#tab5" data-toggle="tab">文献</a></li>
+            <li><a href="#tab6" data-toggle="tab">图表</a></li>
         </ul>
         <div class="tab-content">
             <div class="tab-pane active" id="tab1">
@@ -25,7 +25,7 @@ if (isset($_POST['submit'])) {
                 $query = "SELECT * FROM segment where title like '%$keywords%' or content like '%$keywords%' ORDER BY title ASC LIMIT 0,50";
                 $result = mysqli_query($dbc, $query) or die('Error querying database.');
                 while ($row = mysqli_fetch_array($result)) {
-                    render_segment_summary($dbc, $row);                      
+                    render_segment_summary($dbc, $row);
                 }
                 ?>
 
@@ -40,7 +40,27 @@ if (isset($_POST['submit'])) {
                 <p>患者.</p>
             </div>
             <div class="tab-pane" id="tab5">
-                <p>文献.</p>
+                <?php
+                $url = 'http://s.wanfangdata.com.cn/sru/paper.ashx?operation=searchRetrieve&query=' . $keywords . '%20sortby%20relevance&maximumRecords=10&startRecord=1&version=1.2';
+                
+                $xml = simplexml_load_file($url);
+
+                $feed = file_get_contents($url);
+                $feed = str_replace('srw_dc:dc xmlns:dc="info:srw/schema/1/dc-v1.1" xmlns:srw_dc="info:srw/schema/1/dc-v1.1"', 'srw', $feed);
+                $feed = str_replace('srw_dc:dc', 'srw', $feed);
+                $feed = str_replace('dc:', '', $feed);
+                $xml = new SimpleXmlElement($feed);
+                foreach ($xml->records->record as $r) {
+                    $x = $r->recordData->srw;
+                    $title = $x->title;
+                    $Identifier = $x->Identifier; 
+                  
+                    echo '<h4><a href="http://d.wanfangdata.com.cn/'.$Identifier.'.aspx">'.$title.'</a></h4>';
+                    echo '<p>'.$x->Creator.'等发表于'.$x->Date.'</p>';      
+                    
+                    echo "<p>" . tcmks_substr($x->Description, $word_count=100) . "...</p>";
+                }
+                ?>
             </div>
             <div class="tab-pane" id="tab6">
                 <p>图表.</p>
