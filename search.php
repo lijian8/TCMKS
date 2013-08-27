9499a60c841c9deaab0c2e9638ca7900d5a159ef
Tong Yu <yutongoracle@hotmail.com>
2013-08-27 12:05:48 +0800

    change button size.

--- search.php
+++ search.php
@@ -20,14 +20,14 @@ if (isset($_GET['keywords'])) {
 
     <div class="row">
         <div class="span8">
-            <div class="tabbable tabs-left">
+            <div class="tabbable">
                 <ul class="nav nav-tabs">
-                    <li class="active"><a href="#tab1" data-toggle="tab">所有主题</a></li>
-                    <li><a href="#tab2" data-toggle="tab">成人</a></li>
-                    <li><a href="#tab3" data-toggle="tab">儿科</a></li>
-                    <li><a href="#tab4" data-toggle="tab">患者</a></li>
+                    <li class="active"><a href="#tab1" data-toggle="tab">主题综述</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Wikipedia</a></li>
+                    <li><a href="#tab3" data-toggle="tab">百度搜索</a></li>
+                    <li><a href="#tab4" data-toggle="tab">互动百科</a></li>
                     <li><a href="#tab5" data-toggle="tab">文献</a></li>
-                    <li><a href="#tab6" data-toggle="tab">图表</a></li>
+                    <li><a href="#tab6" data-toggle="tab">图表</a></li>                        
                 </ul>
                 <div class="tab-content">
                     <div class="tab-pane active" id="tab1">
@@ -41,13 +41,72 @@ if (isset($_GET['keywords'])) {
 
                     </div>
                     <div class="tab-pane" id="tab2">
-                        <p>成人.</p>
+
+                        <iframe src="<?php echo 'http://zh.wikipedia.org/wiki/' . $keywords; ?>" width="100%" height="1000"> 
+                        Wikipedia搜索结果 
+                        </iframe>
                     </div>
                     <div class="tab-pane" id="tab3">
-                        <p>儿科.</p>
+
+                        <iframe src="<?php echo 'http://www.baidu.com/s?tn=baiduhome_pg&ie=utf-8&bs=iframe%E7%94%A8%E6%B3%95&f=8&rsv_bp=1&rsv_spt=1&wd=' . $keywords; ?>" width="100%" height="1000"> 
+                        百度搜索结果 
+                        </iframe>
                     </div>
                     <div class="tab-pane" id="tab4">
-                        <p>患者.</p>
+
+                        <iframe src="<?php echo 'http://www.baike.com/wiki/' . $keywords; ?>" width="100%" height="1000"> 
+                        互动百科搜索结果 
+                        </iframe>
+
+
+                        <?php
+                        /*
+                          $url = "http://www.google.com/search?sclient=psy-ab&hl=en&site=&source=hp&q=$keywords";
+                          $cookie_file = dirname(__FILE__) . "/temp/google.txt";
+
+                          $ch = curl_init();
+
+                          curl_setopt($ch, CURLOPT_URL, $url);
+
+                          curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+
+                          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+                          curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+                          curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
+
+                          $contents = curl_exec($ch);
+
+                          curl_close($ch);
+                          //echo $contents;
+                          preg_match_all("/<body(.*?)body>/is", $contents, $body);
+                          //echo $contents;
+                          echo '<iframe   width="100%" height="1000">';
+
+                          echo $contents;
+
+                          // preg_match_all("/<title(.*?)</title>/is", $contents, $title);
+                          //preg_match_all("/<meta(.*?)>/is", $contents, $meta);
+                          //
+                          //echo 'title：' . strip_tags($title[0][0]) . '<br><br>';
+
+                          for ($i = 0; $i < count($meta[0]); $i++) {
+                          if (preg_match("/keywords/i", $meta[0][$i])) {
+                          preg_match_all('/content="(.*?)"/is', $meta[0][$i], $keywords);
+                          }
+                          if (preg_match("/description/i", $meta[0][$i])) {
+                          preg_match_all('/content="(.*?)"/is', $meta[0][$i], $description);
+                          }
+                          }
+                          echo 'keywords：' . strip_tags($keywords[1][0]) . '<br><br>';
+                          echo 'description：' . strip_tags($description[1][0]) . '<br><br>';
+                         * 
+                         */
+                        // echo 'body：' . strip_tags($body[0][0]);
+                        //echo  $body[0][0];
+                        ?>
+                        </iframe>
                     </div>
                     <div class="tab-pane" id="tab5">
                         <?php
@@ -73,17 +132,60 @@ if (isset($_GET['keywords'])) {
                         ?>
                     </div>
                     <div class="tab-pane" id="tab6">
-                        <p>图表.</p>
-                    </div>
+                        <?php
+                        $query = "SELECT * FROM `tcmks`.`images` WHERE name like '%$keywords%' or subject like '%$keywords%' or description like '%$keywords%'";
+                        //echo $query;
+                        $result = mysqli_query($dbc, $query) or die('Error querying database.');
+                        $row = mysqli_fetch_array($result);
+                        while ($row) {
+                            echo '<ul class = "thumbnails">';
+
+                            for ($i = 0; $i < 4; $i++) {
+                                if (!$row)
+                                    break;
+                                echo '<li class="span2">';
+                                echo '<div class = "thumbnail">';
+                                echo '<img src="' . IMG_UPLOADPATH . $row['file'] . '"  alt="" /></p>';
+                                echo '<div class = "caption">';
+                                echo '<p><strong>' . $row['name'] . '.&nbsp;</strong></p>';
+                                // echo $row['description'] . '</p>';
+                                // echo '&nbsp;&nbsp';
+                                echo '</div></div></li>';
+                                $row = mysqli_fetch_array($result);
+                            }
+                            echo '</ul>';
+                        }
+                        ?>
+                    </div>  
+                  
+                        <?php
+                        /*
+                        $baike_keywords = iconv('utf-8', 'gbk', $keywords); //将字符转换成GBK编码，若文件为GBK编码可去掉本行
+                        $encode = urlencode($baike_keywords); //对字符进行URL编码
+                        $url = 'http://baike.baidu.com/list-php/dispose/searchword.php?word=' . $encode . '&pic=1';
+                        $get_contents = file_get_contents($url); //获取跳转页内容
+                        $get_contents = iconv('gbk', 'utf-8', $get_contents); //将获取的网页转换成UTF-8编码，若文件为GBK编码可去掉本行
+                        preg_match("/URL=(\S+)'>/s", $get_contents, $out); //获取跳转后URL
+                        $real_link = 'http://baike.baidu.com' . $out[1];
+
+                        //echo $real_link; //输出 http://baike.baidu.com/view/1466380.htm
+                      
+                        <iframe src="<?php echo $real_link; ?>" width="100%" height="1000"> 
+                        互动百科搜索结果 
+                        </iframe>
+                         * 
+                         */
+                          ?>
+                                  
                 </div>
             </div>
         </div>
         <div class="span4">
             <div class ="well">
-                <?php
-                render_entity($dbc, $keywords);
-                echo '<a class="btn btn-primary" href="entity.php?name=' . $keywords . '"><i class="icon-edit icon-white"></i>&nbsp;编辑</a>';
-                ?>
+<?php
+render_entity($dbc, $keywords);
+echo '<a class="btn btn-primary" href="entity.php?name=' . $keywords . '"><i class="icon-edit icon-white"></i>&nbsp;编辑</a>';
+?>
             </div>
         </div>
     </div>    
